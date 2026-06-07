3685ada9e7a530fd486aec0d9d3ca410f3142292
Giorgio Pomettini <giorgio.freespire@gmail.com>
2026-06-07 14:19:38 +0200

    Add fallback font for entries that has UTF-8 names, bumped to 0.2.2

--- platform/playdate/PokeMini_Playdate.c
+++ platform/playdate/PokeMini_Playdate.c
@@ -148,7 +148,7 @@ static void pdopdiag_log_and_reset(unsigned int updates) {
 
 // --- Platform menu stub (required by UI.c) ------------------------------
 
-const char *AppName = "PlayMini 0.2.1 Playdate";
+const char *AppName = "PlayMini 0.2.2 Playdate";
 
 int UIItems_PlatformC(int index, int reason);
 TUIMenu_Item UIItems_Platform[] = {PLATFORMDEF_GOBACK, PLATFORMDEF_SAVEOPTIONS,
